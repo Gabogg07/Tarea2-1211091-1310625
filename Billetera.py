34df3dc139e1cef6c8753699777de894eef9eaae
Gabogg07 <gabrieleduardogg@gmail.com>
2017-02-01 18:27:05 -0400

    Prueba con funcion inicializada

--- Billetera.py
+++ Billetera.py
@@ -7,4 +7,10 @@ Created on Feb 1, 2017
 class Usuario(object):
 
 
-    def __init__(self, params):
+    def __init__(self, nombre,apellido,ci,pin,id):
+        self.Nombre=nombre
+        self.Apellido=apellido
+        self.Cedula=ci
+        self.PIN=pin
+        self.id=id
+        
