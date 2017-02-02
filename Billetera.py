1cae08b2ee528a3733a6ed51b4c5d51422f656b6
Alessandra Marrero <alemarrero16@gmail.com>
2017-02-02 15:00:24 -0400

    Casos de pruebas faltantes, versión final

--- Billetera.py
+++ Billetera.py
@@ -1,8 +1,8 @@
 '''
 Created on Feb 1, 2017
 
-@author: gabriel
-@author: ale
+@author: Gabriel Gutierrez 13-10625
+@author: Alessandra Marrero 12-11091
 '''
 import datetime
 
@@ -17,11 +17,11 @@ class Registro:
         
 class BilleteraElectronica:
     def __init__(self, nombre = None,apellido = None, ci = None,pin = None,id = None):
-        self.Nombre=nombre
-        self.Apellido=apellido
-        self.Cedula=ci
-        self.PIN=pin
-        self.id=id
+        self.Nombre = nombre
+        self.Apellido = apellido
+        self.Cedula = ci
+        self.PIN = pin
+        self.id = id
         self.regRecarga = []
         self.regConsumo = []
         self.saldoTotal = 0
@@ -44,12 +44,14 @@ class BilleteraElectronica:
     def recargar(self, registro):
         if (registro.monto < 0):
             print("El monto debe de ser positivo")
+            assert(registro.monto>=0)
         self.regRecarga.append(registro)
         self.saldoTotal+= registro.monto
 
     def consumir(self, consumo, pin):
         if (pin != self.PIN):
-            return "El PIN no es valido"
+            print( "El PIN no es valido")
+            assert(pin==self.PIN)
         elif (self.saldoTotal < consumo.monto):
             return "Su saldo no es suficiente"
         elif (consumo.monto < 0):
--- TestBilletera.py
+++ TestBilletera.py
@@ -1,8 +1,8 @@
 '''
 Created on Feb 1, 2017
 
-@author: gabriel
-@author: ale
+@author: Gabriel Gutierrez 13-10625
+@author: Alessandra Marrero 12-11091
 '''
 import unittest
 import datetime
@@ -50,30 +50,87 @@ class Test(unittest.TestCase):
         self.persona.recargar(recargar)
         self.assertEqual(self.persona.regRecarga[-1], recargar,"Error en la recarga")
     
+    #Caso Interior
     def testEsquina1Recargar(self):
         recargar=Registro(0.1,datetime.date.today(),150)
         self.persona.recargar(recargar)
         self.assertEqual(self.persona.regRecarga[-1], recargar,"Error en la recarga")
     
+    #Caso Interior
     def testEsquina2Recargar(self):
         num= float("inf")
         recargar=Registro(num-0.1,datetime.date.today(),150)
         self.persona.recargar(recargar)
         self.assertEqual(self.persona.regRecarga[-1], recargar,"Error en la recarga")
     
+    #Se hace una recarga negativa
     def testMaliciaRecarga(self):
         recargar=Registro(-1 ,datetime.date.today(),150)
         try:
             self.persona.recargar(recargar)
         except:
-            print("No paso la prueba , se acepto una recarga negativa")
+            pass
         
+    def testFrontera1Consumir(self):
+        recargar=Registro(10000,datetime.date.today(),150)
+        self.persona.recargar(recargar)
+        consumo = Registro(10000, datetime.date.today(), 200)
+        self.persona.consumir(consumo, 192)
+        self.assertEqual(self.persona.regConsumo[-1], consumo, "Error en el consumo")
     
+    def testFrontera2Consumir(self):
+        num= float("inf")
+        recargar=Registro(num,datetime.date.today(),150)
+        self.persona.recargar(recargar)
+        consumo = Registro(num, datetime.date.today(), 200)
+        self.persona.consumir(consumo, 192)
+        self.assertEqual(self.persona.regConsumo[-1], consumo, "Error en el consumo")
     
+    #Caso Interior
+    def testEsquina1Consumir(self):
+        recargar=Registro(10001,datetime.date.today(),150)
+        self.persona.recargar(recargar)
+        consumo = Registro(10000, datetime.date.today(), 200)
+        self.persona.consumir(consumo, 192)
+        self.assertEqual(self.persona.regConsumo[-1], consumo, "Error en el consumo")
+    
+    #Caso Interior
+    def testEsquina2Consumir(self):
+        num= float("inf")
+        recargar=Registro(num,datetime.date.today(),150)
+        self.persona.recargar(recargar)
+        consumo = Registro(num-1, datetime.date.today(), 200)
+        self.persona.consumir(consumo, 192)
+        self.assertEqual(self.persona.regConsumo[-1], consumo, "Error en el consumo")
+     
+    #Se hace un consumo mayor al saldo disponible
+    def testMaliciaConsumir(self):
+        recargar=Registro(10,datetime.date.today(),150)
+        self.persona.recargar(recargar)
+        consumo = Registro(10000, datetime.date.today(), 200)
+        try:
+            self.persona.consumir(consumo, 102)
+        except:
+            pass
+        
+    #Se hace un consumo negativo
+    def testMalicia1Consumir(self):
+        recargar=Registro(10,datetime.date.today(),150)
+        self.persona.recargar(recargar)
+        consumo = Registro(-10000, datetime.date.today(), 200)
+        try:
+            self.persona.consumir(consumo, 102)
+        except:
+            pass
         
         
-    
-    
+    def testApellidoValido(self):
+        assert(self.persona.apellidoValido()), "Atributo apellido invalido."
+        
+    def testNombreValido(self):
+        assert(self.persona.nombreValido()), "Atributo nombre invalido."
+        
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
