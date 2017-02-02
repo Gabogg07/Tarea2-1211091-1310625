43a776dc7b45986bde4bc784443f7dbf0201f7aa
Alessandra Marrero <alemarrero16@gmail.com>
2017-02-01 20:53:47 -0400

    Prueba sin metodo saldo

--- Billetera.py
+++ Billetera.py
@@ -2,15 +2,23 @@
 Created on Feb 1, 2017
 
 @author: gabriel
+@author: ale
 '''
-
+class Registro:
+    def __init__(self, monto, fecha, idEstablecimiento):
+        self.monto = monto
+        self.fecha = fecha
+        self.idEstablecimiento = idEstablecimiento
+        
 class BilleteraElectronica(object):
-
-
-    def __init__(self, nombre,apellido,ci,pin,id):
+    def __init__(self, nombre = None,apellido = None, ci = None,pin = None,id = None):
         self.Nombre=nombre
         self.Apellido=apellido
         self.Cedula=ci
         self.PIN=pin
         self.id=id
+        self.regRecarga = []
+        self.regConsumo = []
+        self.saldoTotal = 0
         
+    
--- TestBilletera.py
+++ TestBilletera.py
@@ -2,21 +2,26 @@
 Created on Feb 1, 2017
 
 @author: gabriel
+@author: ale
 '''
 import unittest
-from Billetera import BilleteraElectronica
+from Billetera import *
 
 
-class TestBilleteraElectronica(unittest.TestCase):
+class Test(unittest.TestCase):
 
-
-    def testBilletera(self):
+    def setUp(self):
         persona=BilleteraElectronica("Gabriel","Gutierrez",24723958,192,987654321)
+        
+    def testBilletera(self):
         self.assertEqual(persona.Nombre, "Gabriel", "Error en el nombre")
         self.assertEqual(persona.Apellido, "Gutierrez", "Error en el apellido")
         self.assertEqual(persona.Cedula,24723958,"Error en la cedula")
         self.assertEqual(persona.PIN, 192, "Error en el pin")
         self.assertEqual(persona.id, 987654321, "Error en el identificador")
+    
+    def testSaldo(self):
+        self.assertEqual(persona.saldo, 0, "Error al ver el saldo disponible")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
