05fe2ca6551e4aa6bc59e4eea8b91a0122290e8a
Alessandra Marrero <alemarrero16@gmail.com>
2017-02-02 13:16:49 -0400

    Pruebas de definicion y validacion terminadas.

--- Billetera.py
+++ Billetera.py
@@ -26,13 +26,34 @@ class BilleteraElectronica:
         self.regConsumo = []
         self.saldoTotal = 0
         
+    def nombreValido(self):
+        if self.Nombre.isalpha() and self.Nombre is not None:
+            return True
+        else: 
+            return False
+     
+    def apellidoValido(self):
+        if self.Apellido.isalpha() and self.Apellido is not None:
+            return True
+        else: 
+            return False
+        
     def saldo(self):
         return self.saldoTotal
     
     def recargar(self, registro):
+        if (registro.monto < 0):
+            return "La recarga debe ser positiva"
         self.regRecarga.append(registro)
         self.saldoTotal+= registro.monto
 
-    def consumir(self, consumo):
-        self.regConsumo.append(consumo)
-        self.saldoTotal -= consumo.monto        
+    def consumir(self, consumo, pin):
+        if (pin != self.PIN):
+            return "El PIN no es valido"
+        elif (self.saldoTotal < consumo.monto):
+            return "Su saldo no es suficiente"
+        elif (consumo.monto < 0):
+            return "El monto del consumo no puede ser negativo"
+        else:
+            self.regConsumo.append(consumo)
+            self.saldoTotal -= consumo.monto        
--- TestBilletera.py
+++ TestBilletera.py
@@ -15,6 +15,8 @@ class Test(unittest.TestCase):
         self.persona=BilleteraElectronica("Gabriel","Gutierrez",24723958,192,987654321)
         
     def testBilletera(self):
+        assert(self.persona.nombreValido()), "Atributo nombre invalido."
+        assert(self.persona.apellidoValido()), "Atributo apellido invalido."
         self.assertEqual(self.persona.Nombre, "Gabriel", "Error en el nombre")
         self.assertEqual(self.persona.Apellido, "Gutierrez", "Error en el apellido")
         self.assertEqual(self.persona.Cedula,24723958,"Error en la cedula")
@@ -22,19 +24,23 @@ class Test(unittest.TestCase):
         self.assertEqual(self.persona.id, 987654321, "Error en el identificador")
     
     def testSaldo(self):
-        self.assertEqual(self.persona.saldo(), 0, "Error al ver el saldo disponible")
+        self.assertEqual(self.persona.saldo(),0, "Error al ver el saldo disponible")
         
     def testRecargar(self):
-        recargar=Registro(1000,datetime.date.today(),150)
+        recargar=Registro(10000,datetime.date.today(),150)
         self.persona.recargar(recargar)
         self.assertEqual(self.persona.regRecarga[-1], recargar,"Error en la recarga")
         
     def testConsumir(self):
-        consumo = Registro(2000, datetime.date.today(), 200)
-        self.persona.consumir(consumo)
+        recargar=Registro(10000,datetime.date.today(),150)
+        self.persona.recargar(recargar)
+        consumo = Registro(20, datetime.date.today(), 200)
+        self.persona.consumir(consumo, 192)
         self.assertEqual(self.persona.regConsumo[-1], consumo, "Error en el consumo")
         
-
+    
+    
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
